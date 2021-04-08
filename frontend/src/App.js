7a8fd53fb5dc66483d5a0e391a8cf67847348f16
flppetkovski <flp_petkovski@yahoo.com>
2021-04-08 20:33:53 +0200

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <main>
+   <h1> Welcome to Proshop</h1>
+    </main>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
